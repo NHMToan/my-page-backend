61119771656ebbbbcf2f635ad1dba7ca99b45f86
Toan Nguyen <toan.nguyen@servicenode.se>
2022-04-18 11:22:50 +0700

    Up

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,98 @@
-require('dotenv').config()
-import 'reflect-metadata'
-import express from 'express'
-import { createConnection } from 'typeorm'
-import { User } from './entities/User'
-import { createServer } from 'http'
-import { ApolloServer } from 'apollo-server-express'
-import { buildSchema } from 'type-graphql'
+require("dotenv").config();
 import {
-	ApolloServerPluginDrainHttpServer,
-	ApolloServerPluginLandingPageGraphQLPlayground
-} from 'apollo-server-core'
-import { GreetingResolver } from './resolvers/greeting'
-import { UserResolver } from './resolvers/user'
-import { Context } from './types/Context'
-import refreshTokenRouter from './routes/refreshTokenRouter'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import { createServer } from "http";
+import path from "path";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import { User } from "./entities/User";
+import { GreetingResolver } from "./resolvers/greeting";
+import { UserResolver } from "./resolvers/user";
+import refreshTokenRouter from "./routes/refreshTokenRouter";
+import { Context } from "./types/Context";
 const main = async () => {
-	await createConnection({
-		type: 'postgres',
-		database: 'jwt-auth-tut',
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		logging: true,
-		synchronize: true,
-		entities: [User]
-	})
+  const connection = await createConnection({
+    type: "postgres",
+    ...(__prod__
+      ? {
+          url: process.env.DATABASE_URL,
+          extra: {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          },
+          ssl: true,
+        }
+      : {
+          database: "my-page-dev",
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          synchronize: true,
+        }),
+    logging: true,
+    entities: [User],
+    migrations: [path.join(__dirname, "/migrations/*")],
+  });
+  if (__prod__) await connection.runMigrations();
 
-	const app = express()
+  const app = express();
 
-	app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
-	app.use(cookieParser())
+  app.use(
+    cors({
+      origin: __prod__
+        ? process.env.CORS_ORIGIN_PROD
+        : process.env.CORS_ORIGIN_DEV,
+      credentials: true,
+    })
+  );
+  app.use(cookieParser());
 
-	app.use('/refresh_token', refreshTokenRouter)
+  app.use("/refresh_token", refreshTokenRouter);
 
-	const httpServer = createServer(app)
+  const httpServer = createServer(app);
 
-	const apolloServer = new ApolloServer({
-		schema: await buildSchema({
-			validate: false,
-			resolvers: [GreetingResolver, UserResolver]
-		}),
-		plugins: [
-			ApolloServerPluginDrainHttpServer({ httpServer }),
-			ApolloServerPluginLandingPageGraphQLPlayground
-		],
-		context: ({ req, res }): Pick<Context, 'req' | 'res'> => ({ req, res })
-	})
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      validate: false,
+      resolvers: [GreetingResolver, UserResolver],
+    }),
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageGraphQLPlayground,
+    ],
+    context: ({ req, res }): Pick<Context, "req" | "res"> => ({ req, res }),
+  });
 
-	await apolloServer.start()
+  await apolloServer.start();
 
-	apolloServer.applyMiddleware({
-		app,
-		cors: { origin: 'http://localhost:3000', credentials: true }
-	})
+  apolloServer.applyMiddleware({
+    app,
+    cors: {
+      origin: __prod__
+        ? process.env.CORS_ORIGIN_PROD
+        : process.env.CORS_ORIGIN_DEV,
+      credentials: true,
+    },
+  });
 
-	const PORT = process.env.PORT || 4000
+  const PORT = process.env.PORT || 4000;
 
-	await new Promise(resolve =>
-		httpServer.listen({ port: PORT }, resolve as () => void)
-	)
+  await new Promise((resolve) =>
+    httpServer.listen({ port: PORT }, resolve as () => void)
+  );
 
-	// Typically, http://localhost:4000/graphql
-	console.log(
-		`SERVER STARTED ON PORT ${PORT}. GRAPHQL ENDPOINT ON http://localhost:${PORT}${apolloServer.graphqlPath}`
-	)
-}
+  // Typically, http://localhost:4000/graphql
+  console.log(
+    `SERVER STARTED ON PORT ${PORT}. GRAPHQL ENDPOINT ON http://localhost:${PORT}${apolloServer.graphqlPath}`
+  );
+};
 
-main().catch(error => console.log('ERROR STARTING SERVER: ', error))
+main().catch((error) => console.log("ERROR STARTING SERVER: ", error));
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,30 +1,30 @@
-import { Response } from 'express'
-import { Secret, sign } from 'jsonwebtoken'
-import { User } from '../entities/User'
+import { Response } from "express";
+import { Secret, sign } from "jsonwebtoken";
+import { User } from "../entities/User";
 
-export const createToken = (type: 'accessToken' | 'refreshToken', user: User) =>
-	sign(
-		{
-			userId: user.id,
-			...(type === 'refreshToken' ? { tokenVersion: user.tokenVersion } : {})
-		},
-		type === 'accessToken'
-			? (process.env.ACCESS_TOKEN_SECRET as Secret)
-			: (process.env.REFRESH_TOKEN_SECRET as Secret),
-		{
-			expiresIn: type === 'accessToken' ? '15s' : '60m'
-		}
-	)
+export const createToken = (type: "accessToken" | "refreshToken", user: User) =>
+  sign(
+    {
+      userId: user.id,
+      ...(type === "refreshToken" ? { tokenVersion: user.tokenVersion } : {}),
+    },
+    type === "accessToken"
+      ? (process.env.ACCESS_TOKEN_SECRET as Secret)
+      : (process.env.REFRESH_TOKEN_SECRET as Secret),
+    {
+      expiresIn: type === "accessToken" ? "2h" : "7d",
+    }
+  );
 
 export const sendRefreshToken = (res: Response, user: User) => {
-	res.cookie(
-		process.env.REFRESH_TOKEN_COOKIE_NAME as string,
-		createToken('refreshToken', user),
-		{
-			httpOnly: true,
-			secure: true,
-			sameSite: 'lax',
-			path: '/refresh_token'
-		}
-	)
-}
+  res.cookie(
+    process.env.REFRESH_TOKEN_COOKIE_NAME as string,
+    createToken("refreshToken", user),
+    {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+      path: "/refresh_token",
+    }
+  );
+};
